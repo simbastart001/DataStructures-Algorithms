afed82d170cfbdb6cb0e2a0ad77d3b1a8ddcd58b
simbastart001 <dr.simbastart@gmail.com>
2024-09-05 06:20:58 +0200

    Check for ceiling and or floor in a sorted array

--- src/CeilingQuestion.java
+++ src/CeilingQuestion.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class CeilingQuestion {
     public static void main(String[] args) {
-
-        int[] arr = {-21, -24, -6, 0, 10, 23, 64, 77, 324, 8141, 1013, 4351};
-        int target = 23;
-        int ans = basicBinarySearch(arr, target);
-        System.out.println("Index of target is " + ans);
+        int[] arr = {-21, -24, -6, 0, 10, 23, 64, 77, 324, 1013, 4351};
+        int target = 25;
+        int ans = ceiling(arr, target);
+        System.out.println("Ceiling index is " + ans);
     }
 
-    static int basicBinarySearch(int[] arr, int target) {
+    //    return the index of smallest no >= target
+    static int ceiling(int[] arr, int target) {
+//      when target > last number in the array
+        if (target > arr[arr.length - 1]) {
+            return -1;
+        }
+
         int start = 0;
         int end = arr.length - 1;
 //        find whether array is sorted in Ascending order or Descending order
@@ -39,6 +42,6 @@ public class Main {
             }
 
         }
-        return -1;
+        return start;
     }
 }
--- src/FloorQuestion.java
+++ src/FloorQuestion.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class FloorQuestion {
     public static void main(String[] args) {
 
-        int[] arr = {-21, -24, -6, 0, 10, 23, 64, 77, 324, 8141, 1013, 4351};
-        int target = 23;
-        int ans = basicBinarySearch(arr, target);
-        System.out.println("Index of target is " + ans);
+        int[] arr = {-21, -24, -6, 0, 10, 23, 64, 77, 324, 1013, 4351};
+        int target = 25;
+        int ans = floor(arr, target);
+        System.out.println("Floor index is " + ans);
     }
 
-    static int basicBinarySearch(int[] arr, int target) {
+    //    return the index of smallest no <= target
+    static int floor(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
 //        find whether array is sorted in Ascending order or Descending order
@@ -39,6 +38,6 @@ public class Main {
             }
 
         }
-        return -1;
+        return end;
     }
 }
