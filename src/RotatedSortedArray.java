1de18949d6801c91d11f98b853748c90e8f7303d
simbastart001 <dr.simbastart@gmail.com>
2024-09-08 05:47:40 +0200

    Added findPivotWithDuplicates()

--- src/RotatedSortedArray.java
+++ src/RotatedSortedArray.java
@@ -1,7 +1,9 @@
+// https://leet-code.com/problems/search-in-rotated-sorteed-array
+
 public class RotatedSortedArray {
     public static void main(String[] args) {
 
-        int[] arr = {3, 4, 5, 6, 7, 0, 1, 2};
+        int[] arr = {3, 4, 5, 6, 6, 7, 7, 0, 1, 2};
         int thePivot = findPivot(arr);
         System.out.println("The pivot is: " + arr[thePivot]);
 
@@ -16,7 +18,8 @@ public class RotatedSortedArray {
 //    3) else Search in the second half => bS (pivot +1, end)
 
     static int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
+//        int pivot = findPivot(nums);
+        int pivot = findPivotWithDuplicates(nums);
 
 //        if pivot not found, it means array is not rotated
         if (pivot == -1) {
@@ -85,5 +88,49 @@ public class RotatedSortedArray {
             }
         }
         return -1;
+    }
+
+    static int findPivotWithDuplicates(int[] arr) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+//            case1:
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+//            case2:
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+//            case3:
+//            if elements at start, middle and end are equal :: skip the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+//                skip the duplicates
+
+//     todo:           NB: what if the duplicates are the pivots!!!
+//                check if start is not pivot
+                if (arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+
+//                check if end is not pivot
+                if (arr[end] > arr[end - 1]) {
+                    return end;
+                }
+                end--;
+            }
+//            left side is sorted, pivot should be on the right side
+            else if (arr[start] < arr[mid] || arr[start] == arr[mid] && arr[mid] > arr[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return Integer.MAX_VALUE;
     }
 }
